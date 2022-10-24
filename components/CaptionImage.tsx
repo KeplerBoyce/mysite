153da91905fe1662c6587e3e6015319393e965e4
KeplerBoyce <keplerboy@gmail.com>
2022-10-23 23:32:47 -0700

    fix images

--- components/CaptionImage.tsx
+++ components/CaptionImage.tsx
@@ -1,11 +1,9 @@
-import Image from "next/image";
-
 export default function CaptionImage(props: {image: string, caption: string}) {
     const { image, caption } = props;
 
     return (
         <div className="flex flex-col gap-2 max-w-md">
-            <Image src={image} alt={caption} />
+            <img src={image} alt={caption} />
             <p className="text-sm text-center italic">
                 {caption}
             </p>
